128624516396e5690f2c9e29831a4d9af26604c3
Monigonza <monica.lahibeth@gmail.com>
2025-04-29 16:04:40 -0400

    Actualizar archivo Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        double temperaturaEnCelsius = 30.4;
+        double temperaturaEnFahrenheit = (temperaturaEnCelsius * 1.8) + 32;
+
+        String mensaje = String.format("\nLa temperatura de %1.1f grados Celsius es equivalente a %3.3f grados Fahrenheit.\n", temperaturaEnCelsius, temperaturaEnFahrenheit);
+        System.out.println(mensaje);
+
+        int temperaturaEnFahrenheitEntero = (int) temperaturaEnFahrenheit;
+        System.out.println("La temperatura en Fahrenheit en grados enteros es: " + temperaturaEnFahrenheitEntero);
     }
 }
